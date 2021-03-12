2700b0a41dcdeb5f73f597461f56b16a111aa1ea
Tony <40070247+desulaid@users.noreply.github.com>
2021-03-12 11:57:34 +0300

    добавление студентов в группы

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -17,25 +17,33 @@ def index():
     return render_template('dashboard/index.html')
 
 
-@dashboard.route('/group', methods=['GET', 'POST'])
+@dashboard.route('/group', methods=['GET'])
 @login_required
 def group():
-    group = Group.query.filter_by(user_id=current_user.id).first()
+    user_group = Group.query.filter_by(user_id=current_user.id).first()
     group_exist = False
-    form_values = []
-    if group:
-        group_exist = True
-
-    if request.method == 'POST':
-        id = 0
-        if not group:
-            id = Group(user_id=current_user.id)
-            db.session.add(id)
-            db.session.commit()
-        else:
-            # id =
-            pass
+    group_students = Student.query.filter_by(group_id=user_group.id).all()
+    form_values = [item.name for item in group_students]
 
     context = dict(group_exist=group_exist, data=dumps(form_values))
 
     return render_template('dashboard/group.html', **context)
+
+
+@dashboard.route('/group-add-items', methods=['POST'])
+@login_required
+def add_items():
+    user_group = Group.query.filter_by(user_id=current_user.id).first()
+
+    if not user_group:
+        Group(user_id=current_user.id)
+        db.session.add(id)
+        db.session.commit()
+    else:
+        Student.query.delete()
+        for key in request.form:
+            db.session.add(Student(name=request.form[key], group_id=user_group.id))
+
+        db.session.commit()
+    return redirect(url_for('.group'))
+
