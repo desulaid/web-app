59908b8fe6bed6a9a8a9dddf8c16cc2a9134f542
Desulaid <desulaid@yandex.ru>
2021-05-06 19:59:32 +0300

    redirect

--- app/profile/login_manager.py
+++ app/profile/login_manager.py
@@ -4,6 +4,9 @@ from app.database.models import Profile
 
 login_manager = LoginManager()
 
+login_manager.login_view = 'profile.login'
+login_manager.login_message = 'Для начала авторизуйтесь'
+login_manager.login_message_category = 'warning'
 
 class UserLogin(Profile, UserMixin):
     def is_authenticated(self):
