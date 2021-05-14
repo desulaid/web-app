293af80f7f3eed03248c32e6cd4042473b3bc274
Tony <40070247+desulaid@users.noreply.github.com>
2021-05-14 14:51:07 +0300

    update

--- app/info/views.py
+++ app/info/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, render_template, url_for
+from flask_login import current_user as user
+
 
 info = Blueprint('info', __name__, template_folder='templates')
 
@@ -6,6 +8,11 @@ info = Blueprint('info', __name__, template_folder='templates')
 @info.route('/info', methods=['GET'])
 @info.route('/', methods=['GET'])
 def index():
-    url = url_for('dashboard.group')
 
-    return f'<a href="{url}">Перейти, пока что</a>'
+    context = dict(
+        title='Статистика',
+        header='Статистика по посещаемым занятиям',
+        user=user
+    )
+
+    return render_template('info/index.html', **context)
