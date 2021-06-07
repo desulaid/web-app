c3e708a05b0c87831e267b1d68f793e96899b3b9
Tony <40070247+desulaid@users.noreply.github.com>
2021-06-07 09:41:22 +0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 from app import create_app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run()
-
-# TODO:
-# - отчеты по студенту, неделе, месяцу
-# - указывать время занятия
-# - упросить добавление записей (ибо телефончик)
+app = create_app()
