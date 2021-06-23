938241ef59d472c16232c3c5096759193ad6a3c7
Tony <40070247+desulaid@users.noreply.github.com>
2021-06-23 12:33:40 +0300

    Update cli.py

--- app/cli.py
+++ app/cli.py
@@ -2,7 +2,10 @@ from click import echo, argument
 from flask import current_app as app
 from flask.cli import AppGroup
 
-from app.database import db, Role, Group, Teacher, Profile
+from app.database import db, Role, Group, Teacher, Profile, Student, Post, Task
+
+import random
+from datetime import datetime, time
 
 cli = AppGroup('database')
 
@@ -52,4 +55,60 @@ def cli_data() -> None:
 
     db.session.commit()
 
+    echo('Данные успешно внесены в базу.')
+
+
+@cli.command('exam')
+def cli_exam() -> None:
+    data: list = app.config['EXAM_DATA']
+
+    for key in data:
+        if key == 'Студенты':
+            for item in data[key]:
+                db.session.add(Student(name=item,
+                                       profile_id=1))
+            db.session.commit()
+        elif key == 'Предметы':
+            for day in range(1, 29):                
+                if (day == 6 or day == 7 or
+                    day == 13 or day == 14 or
+                    day == 20 or day == 21 or
+                    day == 27 or day == 28):
+                        continue
+                
+                hour = 8
+                minute = 00
+
+                date = datetime(2021, 6, day, hour, minute, 0)                 
+                    
+                for lesson in data[key][f'{date.weekday()}']:                                      
+                    if hour >= 18:
+                        hour = 8
+
+                    if minute >= 59:
+                        minute = 0
+                    
+
+                    timea = time(hour=hour, minute=minute)
+                
+                    post = Post(profile=1)
+
+                    for i, student in enumerate(data['Студенты'], start=1):  
+                        attended = bool(random.getrandbits(1))
+                        comment = ''
+                        
+                        if not attended:
+                            comment == 'Заболел'
+
+                        post.tasks.append(Task(title=lesson,
+                               attended=attended,
+                               comment=comment,
+                               datetime=datetime.combine(date.date(), timea),
+                               student_id=int(i)))
+                        db.session.add(post)
+                    minute = 15
+                    hour += 2
+
+                db.session.commit()
+            
     echo('Данные успешно внесены в базу.')
